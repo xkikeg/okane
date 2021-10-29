32d1895476f530e73d215dcc4f63cf76cac6adce
kikeg <kikeg@kikeg.com>
2021-10-29 23:04:24 +0000

    Add effective date (aka auxiliary date).

--- src/import/single_entry.rs
+++ src/import/single_entry.rs
@@ -5,36 +5,48 @@ use chrono::NaiveDate;
 
 /// Represents single-entry transaction, associated with the particular account.
 pub struct Txn {
-    /// Date of the transaction happened.
-    pub date: NaiveDate,
+    /// Date when the transaction happened.
+    date: NaiveDate,
+
+    /// Date when the transaction took effects (i.e. actually paid / transfered).
+    effective_date: Option<NaiveDate>,
 
     /// Code of the transcation for tracking.
-    pub code: Option<String>,
+    code: Option<String>,
 
     /// Payee (or payer) of the transaction.
-    pub payee: String,
+    payee: String,
 
     /// Destination account.
-    pub dest_account: Option<String>,
+    dest_account: Option<String>,
 
     /// Amount of the transaction, applied for the associated account.
     /// For bank account, positive means deposit, negative means withdraw.
     /// For credit card account, negative means expense, positive means payment to the card.
-    pub amount: data::Amount,
+    amount: data::Amount,
 
-    pub balance: Option<data::Amount>,
+    balance: Option<data::Amount>,
 }
 
 impl Txn {
     pub fn new(date: NaiveDate, payee: &str, amount: data::Amount) -> Txn {
         Txn {
             date,
+            effective_date: None,
             code: None,
             payee: payee.to_string(),
             dest_account: None,
             amount,
             balance: None,
         }
+    }
+
+    /// Set effective date, only when it's different from date.
+    pub fn effective_date(&mut self, effective_date: NaiveDate) -> &mut Txn {
+        if self.date != effective_date {
+            self.effective_date = Some(effective_date);
+        }
+        self
     }
 
     pub fn code_option<'a>(&'a mut self, code: Option<&str>) -> &'a mut Txn {
@@ -106,10 +118,48 @@ impl Txn {
         }
         Ok(data::Transaction {
             date: self.date,
+            effective_date: self.effective_date,
             clear_state: data::ClearState::Cleared,
             code: self.code.clone(),
             payee: self.payee.clone(),
             posts,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use rust_decimal_macros::dec;
+
+    #[test]
+    fn test_effective_date_not_set_same_date() {
+        let mut txn = Txn::new(
+            NaiveDate::from_ymd(2021, 10, 1),
+            "foo",
+            data::Amount {
+                commodity: "JPY".to_string(),
+                value: dec!(10),
+            },
+        );
+        txn.effective_date(NaiveDate::from_ymd(2021, 10, 1));
+
+        assert_eq!(txn.effective_date, None);
+    }
+
+    #[test]
+    fn test_effective_date_set_different_date() {
+        let mut txn = Txn::new(
+            NaiveDate::from_ymd(2021, 10, 1),
+            "foo",
+            data::Amount {
+                commodity: "JPY".to_string(),
+                value: dec!(10),
+            },
+        );
+        txn.effective_date(NaiveDate::from_ymd(2021, 10, 2));
+
+        assert_eq!(txn.effective_date, Some(NaiveDate::from_ymd(2021, 10, 2)));
     }
 }
