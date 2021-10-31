050b8f52e26459a5f4685e10cc880acdf6037f5f
kikeg <kikeg@kikeg.com>
2021-10-31 23:57:22 +0000

    Reduce number of CLI end-to-end testing, factor out as test_cli_import.
    Currently CSV all use-case has end-to-end testing, which is too much.

---
+++ tests/test_cli_import.rs
@@ -0,0 +1,20 @@
+mod testing;
+
+use std::io::{self, Write};
+
+#[test]
+fn test_import_success() {
+    let config = testing::TESTDATA_DIR.join("test_config.yml");
+    let input = testing::TESTDATA_DIR.join("index_amount.csv");
+    let want = testing::read_as_utf8("index_amount.ledger").unwrap();
+
+    let result = assert_cmd::Command::new(&*testing::BIN_PATH)
+        .args(&[config, input])
+        .assert()
+        .success();
+
+    let output = result.get_output();
+    io::stderr().write_all(&output.stderr).unwrap();
+    let stdout = std::str::from_utf8(&output.stdout).unwrap();
+    assert_eq!(want, stdout);
+}
--- tests/testing/mod.rs
+++ tests/testing/mod.rs
@@ -1,9 +1,16 @@
 use std::path::{Path, PathBuf};
 
+use std::fs;
+
 use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref TESTDATA_DIR: PathBuf =
         Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/testdata");
     pub static ref BIN_PATH: PathBuf = assert_cmd::cargo::cargo_bin(assert_cmd::crate_name!());
+}
+
+/// Returns content of testdata directory, as UTF-8.
+pub fn read_as_utf8(filename: &str) -> std::io::Result<String> {
+    fs::read_to_string(TESTDATA_DIR.join(filename))
 }
