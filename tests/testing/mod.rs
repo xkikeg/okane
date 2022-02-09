b56cdd037c5279c6ee2cdb0cc44d3b54b0eb1f32
kikeg <kikeg@kikeg.com>
2022-02-09 23:11:37 +0100

    Support windows by path / CRLF.

--- tests/testing/mod.rs
+++ tests/testing/mod.rs
@@ -5,12 +5,15 @@ use std::fs;
 use lazy_static::lazy_static;
 
 lazy_static! {
-    pub static ref TESTDATA_DIR: PathBuf =
-        Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/testdata");
+    pub static ref TESTDATA_DIR: PathBuf = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("tests")
+        .join("testdata");
     pub static ref BIN_PATH: PathBuf = assert_cmd::cargo::cargo_bin(assert_cmd::crate_name!());
 }
 
 /// Returns content of testdata directory, as UTF-8.
 pub fn read_as_utf8(filename: &str) -> std::io::Result<String> {
-    fs::read_to_string(TESTDATA_DIR.join(filename))
+    // Needs to replace CRLF to LF for Windows, as all files will have CRLF
+    // due to git config core.autocrlf.
+    fs::read_to_string(TESTDATA_DIR.join(filename)).map(|s| s.replace("\r\n", "\n"))
 }
