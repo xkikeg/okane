674b269d2bece2880d9bfa23821713f686f38a26
kikeg <kikeg@kikeg.com>
2024-05-28 17:16:50 +0200

    Add a very simple test case for load.

--- core/src/eval.rs
+++ core/src/eval.rs
@@ -43,3 +43,40 @@ pub enum LoadError {
     #[error("unexpected include path {0}")]
     IncludePath(PathBuf),
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::repl::parser::parse_ledger;
+
+    use indoc::indoc;
+    use pretty_assertions::assert_eq;
+    use std::path::Path;
+
+    #[test]
+    fn load_valid_input() {
+        let root = Path::new(env!("CARGO_MANIFEST_DIR")).join("testdata/root.ledger");
+        let got = load(&root).unwrap();
+        let want = parse_ledger(indoc! {"
+            account Expenses:Grocery
+                note スーパーマーケットで買ったやつ全部
+                ; comment
+                alias Expenses:CVS
+
+            2024/1/1 Initial Balance
+                Equity:Opening Balance       -1000.00 CHF
+                Assets:Bank:ZKB               1000.00 CHF
+
+            2024/1/1 * SBB CFF FFS
+                Assets:Bank:ZKB                 -5.60 CHF
+                Expenses:Travel:Train            5.60 CHF
+
+            2024/5/1 * Migros
+                Expenses:Grocery               -10.00 CHF
+                Assets:Bank:ZKB                 10.00 CHF
+        "})
+        .unwrap();
+        assert_eq!(got, want);
+    }
+}
