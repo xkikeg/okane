b78b9b05e80337f9273c15d774a9d9768f24b217
kikeg <kikeg@kikeg.com>
2024-06-12 16:02:17 +0200

    Reduce testing input size for the benchmark.

--- core/benches/testing/mod.rs
+++ core/benches/testing/mod.rs
@@ -39,6 +39,12 @@ pub enum ExampleInputError {
 
 const CLEANUP_KEY: &str = "OKANE_BENCH_CLEANUP";
 
+const YEAR_BEGIN: i32 = 2015;
+const YEAR_END: i32 = 2025;
+const NUM_SUB_FILES: usize = 16;
+const NUM_THREADS: usize = 2;
+const NUM_TRANSACTIONS_PER_FILE: usize = 500;
+
 impl ExampleInput {
     /// Create an example used for benchmarks.
     /// Created example is left as-is, unless `OKANE_BENCH_CLEANUP` is set.
@@ -82,18 +88,18 @@ impl ExampleInput {
         // Assuming you have 50 accounts & 100 years of records.
         let mut tasks = Vec::new();
         let (tx, rx) = mpsc::channel();
-        for i1 in 0..5 {
+        for i1 in 0..NUM_THREADS {
             let thread_tx = tx.clone();
             let rootdir = rootdir.clone();
             tasks.push(std::thread::spawn(move || {
-                let mut years = Vec::with_capacity(100);
-                for j in 2000..2100 {
+                let mut years = Vec::with_capacity((YEAR_END - YEAR_BEGIN) as usize);
+                for j in YEAR_BEGIN..YEAR_END {
                     let year = Year(j);
                     years.push(year);
                 }
                 let years = years.as_slice();
-                for i2 in 0..10 {
-                    let i = i1 * 10 + i2;
+                for i2 in 0..NUM_SUB_FILES / NUM_THREADS {
+                    let i = i1 * NUM_SUB_FILES / NUM_THREADS + i2;
                     let dirname = dir_name(i);
                     let subdirpath = rootdir.join(&dirname);
                     let ret = || -> Result<(), io::Error> {
@@ -111,7 +117,7 @@ impl ExampleInput {
             rx.recv_timeout(std::time::Duration::from_secs(150))
                 .expect("Can't wait 1 minute on the recv task")?;
         }
-        prepare_root_file(&rootfile, 0..50)?;
+        prepare_root_file(&rootfile, 0..NUM_SUB_FILES)?;
         for jh in tasks.into_iter() {
             jh.join().expect("thread join must not fail");
         }
@@ -130,7 +136,7 @@ impl ExampleInput {
     }
 }
 
-fn prepare_root_file(rootfile: &Path, dirs: std::ops::Range<i32>) -> Result<(), std::io::Error> {
+fn prepare_root_file(rootfile: &Path, dirs: std::ops::Range<usize>) -> Result<(), std::io::Error> {
     let mut w = BufWriter::new(File::create(rootfile)?);
     for dir in dirs {
         writeln!(w, "include {}.ledger", dir_name(dir))?;
@@ -153,22 +159,22 @@ fn prepare_middle_file(
     w.into_inner()?.sync_all()
 }
 
-fn payee(dir: i32, year: Year, i: u32) -> String {
+fn payee(dir: usize, year: Year, i: usize) -> String {
     format!(
         "Random payee {}",
-        (dir * 31 + year.0 * 17 + (i as i32) * 7) % 13
+        ((dir as i32) * 31 + year.0 * 17 + (i as i32) * 7) % 13
     )
 }
 
-fn prepare_leaf_file(target: &Path, dir: i32, year: Year) -> Result<(), std::io::Error> {
+fn prepare_leaf_file(target: &Path, dir: usize, year: Year) -> Result<(), std::io::Error> {
     let mut w = BufWriter::new(File::create(target)?);
-    for i in 0..1000 {
-        let ordinal = i * 365 / 1000 + 1;
+    for i in 0..NUM_TRANSACTIONS_PER_FILE {
+        let ordinal = (i * 365 / NUM_TRANSACTIONS_PER_FILE + 1) as u32;
         let date = NaiveDate::from_yo_opt(year.0, ordinal)
             .expect("must be a valid date")
             .format("%Y/%m/%d");
         let payee = payee(dir, year, i);
-        let pseudo = (dir * 31 + year.0 * 17 + (i as i32) * 7) % 13;
+        let pseudo = ((dir as i32) * 31 + year.0 * 17 + (i as i32) * 7) % 13;
         let (other_account, other_amount, amount) = match pseudo {
             0..=10 => (
                 format!("Expenses:Type{}", pseudo),
@@ -187,8 +193,8 @@ fn prepare_leaf_file(target: &Path, dir: i32, year: Year) -> Result<(), std::io:
     w.into_inner()?.sync_all()
 }
 
-fn dir_name(i: i32) -> String {
-    format!("sub{:02}", i)
+fn dir_name(i: usize) -> String {
+    format!("sub-{:02}", i)
 }
 
 fn leaf_file(year: Year) -> String {
