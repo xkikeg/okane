e2a93e12ba34e14d090464a8a5187f4130e13079
kikeg <kikeg@kikeg.com>
2024-09-20 23:02:24 +0200

    Add TrackedSpan and ParsedSpan to deduce the actual span.

--- core/src/syntax/tracked.rs
+++ core/src/syntax/tracked.rs
@@ -23,7 +23,7 @@ impl Decoration for Tracking {
         use winnow::Parser;
         parser
             .with_span()
-            .map(|(value, position)| Tracked { position, value })
+            .map(|(value, span)| Tracked::new(value, TrackedSpan(span)))
     }
 }
 
@@ -33,15 +33,47 @@ pub type Posting<'i> = super::Posting<'i, Tracking>;
 pub type PostingAmount<'i> = super::PostingAmount<'i, Tracking>;
 pub type Lot<'i> = super::Lot<'i, Tracking>;
 
+/// Span of the tracked position.
+/// Only useful within [ParsedContext][crate::parse::ParsedContext].
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct TrackedSpan(Range<usize>);
+
+impl TrackedSpan {
+    /// Returns the implementation of the span.
+    /// Note this is relative to the original string,
+    /// not the local index in the string.
+    pub(crate) fn as_range(&self) -> Range<usize> {
+        self.0.clone()
+    }
+
+    /// Creates an instance, only for unit test.
+    #[cfg(test)]
+    pub fn new(span: Range<usize>) -> TrackedSpan {
+        TrackedSpan(span)
+    }
+}
+
 /// Tracked provides the struct with maybe position tracking.
 #[derive(Debug, PartialEq, Eq)]
 pub struct Tracked<T> {
-    position: Range<usize>,
     value: T,
+    span: TrackedSpan,
 }
 
 impl<T> AsUndecorated<T> for Tracked<T> {
     fn as_undecorated(&self) -> &T {
         &self.value
+    }
+}
+
+impl<T> Tracked<T> {
+    /// Returns span for the tracked position.
+    pub fn span(&self) -> TrackedSpan {
+        self.span.clone()
+    }
+
+    /// Wraps another value with the same span.
+    pub fn new(value: T, span: TrackedSpan) -> Self {
+        Self { value, span }
     }
 }
