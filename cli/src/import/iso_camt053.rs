6c65509986e13de81c8d347be72ec15949fc4f48
kikeg <kikeg@kikeg.com>
2024-10-23 17:54:02 +0200

    Remove unnecessary trait.
    import is statically dispatched, no need to be trait.

--- cli/src/import/iso_camt053.rs
+++ cli/src/import/iso_camt053.rs
@@ -13,122 +13,114 @@ use super::extract;
 use super::single_entry::{self, CommodityPair};
 use super::ImportError;
 
-pub struct IsoCamt053Importer {}
-
-impl super::Importer for IsoCamt053Importer {
-    fn import<R>(
-        &self,
-        r: R,
-        config: &config::ConfigEntry,
-    ) -> Result<Vec<single_entry::Txn>, ImportError>
-    where
-        R: std::io::Read,
-    {
-        let extractor: extract::Extractor<FieldMatch> = (&config.rewrite).try_into()?;
-        let mut buf = std::io::BufReader::new(r);
-        let doc: xmlnode::Document = quick_xml::de::from_reader(&mut buf)?;
-        let mut res = Vec::new();
-        for stmt in doc.bank_to_customer.statements {
-            if let Some(opening_balance) = find_balance(&stmt, xmlnode::BalanceCode::Opening) {
-                if let Some(first) = stmt.entries.first() {
-                    let mut txn = single_entry::Txn::new(
-                        first.value_date.date,
-                        "Initial Balance",
-                        OwnedAmount {
-                            commodity: opening_balance.commodity.clone(),
-                            value: Decimal::ZERO,
-                        },
-                    );
-                    txn.dest_account("Equity:Adjustments");
-                    txn.balance(opening_balance);
-                    res.push(txn);
-                }
-            }
-            let closing_balance = find_balance(&stmt, xmlnode::BalanceCode::Closing);
-            for entry in stmt.entries {
-                if entry.details.transactions.is_empty() {
-                    // TODO(kikeg): Fix this code repetition.
-                    let amount = entry.amount.to_data(entry.credit_or_debit.value);
-                    let fragment = extractor.extract((&entry, None));
-                    if fragment.payee.is_none() {
-                        log::warn!("payee not set @ {:?} {:?}", entry.booking_date, amount);
-                    } else if fragment.account.is_none() {
-                        log::warn!(
-                            "account not set @ {:?} {:?} {}",
-                            entry.booking_date,
-                            amount,
-                            fragment.payee.unwrap()
-                        );
-                    }
-                    let mut txn = single_entry::Txn::new(
-                        entry.value_date.date,
-                        fragment.payee.unwrap_or("unknown payee"),
-                        amount,
-                    );
-                    txn.effective_date(entry.booking_date.date)
-                        .dest_account_option(fragment.account);
-                    if !fragment.cleared {
-                        txn.clear_state(syntax::ClearState::Pending);
-                    }
-                    add_charges(&mut txn, config, &entry.charges)?;
-                    res.push(txn);
-                }
-                for transaction in &entry.details.transactions {
-                    let amount = transaction
-                        .amount
-                        .to_data(transaction.credit_or_debit.value);
-                    let fragment = extractor.extract((&entry, Some(transaction)));
-                    let code = transaction.refs.account_servicer_reference.as_deref();
-                    if fragment.payee.is_none() {
-                        log::warn!("payee not set @ {:?}", code);
-                    } else if fragment.account.is_none() {
-                        log::warn!("account not set @ {:?} {}", code, fragment.payee.unwrap());
-                    }
-                    let mut txn = single_entry::Txn::new(
-                        entry.value_date.date,
-                        fragment.payee.unwrap_or("unknown payee"),
-                        amount,
-                    );
-                    txn.effective_date(entry.booking_date.date)
-                        .code_option(code)
-                        .dest_account_option(fragment.account);
-                    if !fragment.cleared {
-                        txn.clear_state(syntax::ClearState::Pending);
-                    }
-                    if let Some(amount_details) = transaction.amount_details.as_ref() {
-                        if transaction.amount != amount_details.transaction.amount {
-                            if let Some(exchange) =
-                                amount_details.transaction.currency_exchange.as_ref()
-                            {
-                                txn.add_rate(
-                                    CommodityPair {
-                                        source: exchange.source_currency.clone(),
-                                        target: exchange.target_currency.clone(),
-                                    },
-                                    exchange.exchange_rate.value,
-                                )?;
-                            }
-                            txn.transferred_amount(
-                                amount_details
-                                    .transaction
-                                    .amount
-                                    .to_data(transaction.credit_or_debit.value),
-                            );
-                        }
-                    }
-                    add_charges(&mut txn, config, &entry.charges)?;
-                    add_charges(&mut txn, config, &transaction.charges)?;
-                    res.push(txn);
-                }
-            }
-            if let Some(last_txn) = res.last_mut() {
-                if let Some(b) = closing_balance {
-                    last_txn.balance(b);
-                }
+pub fn import<R>(r: R, config: &config::ConfigEntry) -> Result<Vec<single_entry::Txn>, ImportError>
+where
+    R: std::io::Read,
+{
+    let extractor: extract::Extractor<FieldMatch> = (&config.rewrite).try_into()?;
+    let mut buf = std::io::BufReader::new(r);
+    let doc: xmlnode::Document = quick_xml::de::from_reader(&mut buf)?;
+    let mut res = Vec::new();
+    for stmt in doc.bank_to_customer.statements {
+        if let Some(opening_balance) = find_balance(&stmt, xmlnode::BalanceCode::Opening) {
+            if let Some(first) = stmt.entries.first() {
+                let mut txn = single_entry::Txn::new(
+                    first.value_date.date,
+                    "Initial Balance",
+                    OwnedAmount {
+                        commodity: opening_balance.commodity.clone(),
+                        value: Decimal::ZERO,
+                    },
+                );
+                txn.dest_account("Equity:Adjustments");
+                txn.balance(opening_balance);
+                res.push(txn);
             }
         }
-        Ok(res)
+        let closing_balance = find_balance(&stmt, xmlnode::BalanceCode::Closing);
+        for entry in stmt.entries {
+            if entry.details.transactions.is_empty() {
+                // TODO(kikeg): Fix this code repetition.
+                let amount = entry.amount.to_data(entry.credit_or_debit.value);
+                let fragment = extractor.extract((&entry, None));
+                if fragment.payee.is_none() {
+                    log::warn!("payee not set @ {:?} {:?}", entry.booking_date, amount);
+                } else if fragment.account.is_none() {
+                    log::warn!(
+                        "account not set @ {:?} {:?} {}",
+                        entry.booking_date,
+                        amount,
+                        fragment.payee.unwrap()
+                    );
+                }
+                let mut txn = single_entry::Txn::new(
+                    entry.value_date.date,
+                    fragment.payee.unwrap_or("unknown payee"),
+                    amount,
+                );
+                txn.effective_date(entry.booking_date.date)
+                    .dest_account_option(fragment.account);
+                if !fragment.cleared {
+                    txn.clear_state(syntax::ClearState::Pending);
+                }
+                add_charges(&mut txn, config, &entry.charges)?;
+                res.push(txn);
+            }
+            for transaction in &entry.details.transactions {
+                let amount = transaction
+                    .amount
+                    .to_data(transaction.credit_or_debit.value);
+                let fragment = extractor.extract((&entry, Some(transaction)));
+                let code = transaction.refs.account_servicer_reference.as_deref();
+                if fragment.payee.is_none() {
+                    log::warn!("payee not set @ {:?}", code);
+                } else if fragment.account.is_none() {
+                    log::warn!("account not set @ {:?} {}", code, fragment.payee.unwrap());
+                }
+                let mut txn = single_entry::Txn::new(
+                    entry.value_date.date,
+                    fragment.payee.unwrap_or("unknown payee"),
+                    amount,
+                );
+                txn.effective_date(entry.booking_date.date)
+                    .code_option(code)
+                    .dest_account_option(fragment.account);
+                if !fragment.cleared {
+                    txn.clear_state(syntax::ClearState::Pending);
+                }
+                if let Some(amount_details) = transaction.amount_details.as_ref() {
+                    if transaction.amount != amount_details.transaction.amount {
+                        if let Some(exchange) =
+                            amount_details.transaction.currency_exchange.as_ref()
+                        {
+                            txn.add_rate(
+                                CommodityPair {
+                                    source: exchange.source_currency.clone(),
+                                    target: exchange.target_currency.clone(),
+                                },
+                                exchange.exchange_rate.value,
+                            )?;
+                        }
+                        txn.transferred_amount(
+                            amount_details
+                                .transaction
+                                .amount
+                                .to_data(transaction.credit_or_debit.value),
+                        );
+                    }
+                }
+                add_charges(&mut txn, config, &entry.charges)?;
+                add_charges(&mut txn, config, &transaction.charges)?;
+                res.push(txn);
+            }
+        }
+        if let Some(last_txn) = res.last_mut() {
+            if let Some(b) = closing_balance {
+                last_txn.balance(b);
+            }
+        }
     }
+    Ok(res)
 }
 
 fn find_balance(stmt: &xmlnode::Statement, code: xmlnode::BalanceCode) -> Option<OwnedAmount> {
--- cli/src/import/viseca.rs
+++ cli/src/import/viseca.rs
@@ -14,70 +14,65 @@ use super::single_entry;
 use super::single_entry::CommodityPair;
 use super::ImportError;
 
-pub struct VisecaImporter {}
-
-impl super::Importer for VisecaImporter {
-    fn import<R: std::io::Read>(
-        &self,
-        r: R,
-        config: &config::ConfigEntry,
-    ) -> Result<Vec<single_entry::Txn>, ImportError> {
-        let extractor: extract::Extractor<VisecaMatcher> = (&config.rewrite).try_into()?;
-        let mut parser =
-            parser::Parser::new(std::io::BufReader::new(r), config.commodity.primary.clone());
-        let mut result = Vec::new();
-        while let Some(entry) = parser.parse_entry()? {
-            let fragment = extractor.extract(&entry);
-            let payee = fragment.payee.unwrap_or(entry.payee.as_str());
-            if fragment.account.is_none() {
-                log::warn!(
-                    "account unmatched at line {}, payee={}",
-                    entry.line_count,
-                    payee
-                );
-            }
-            let mut txn = single_entry::Txn::new(
-                entry.date,
-                payee,
-                OwnedAmount {
-                    value: -entry.amount,
-                    commodity: config.commodity.primary.clone(),
-                },
+pub fn import<R: std::io::Read>(
+    r: R,
+    config: &config::ConfigEntry,
+) -> Result<Vec<single_entry::Txn>, ImportError> {
+    let extractor: extract::Extractor<VisecaMatcher> = (&config.rewrite).try_into()?;
+    let mut parser =
+        parser::Parser::new(std::io::BufReader::new(r), config.commodity.primary.clone());
+    let mut result = Vec::new();
+    while let Some(entry) = parser.parse_entry()? {
+        let fragment = extractor.extract(&entry);
+        let payee = fragment.payee.unwrap_or(entry.payee.as_str());
+        if fragment.account.is_none() {
+            log::warn!(
+                "account unmatched at line {}, payee={}",
+                entry.line_count,
+                payee
             );
-            txn.effective_date(entry.effective_date)
-                .dest_account_option(fragment.account);
-            if !fragment.cleared {
-                txn.clear_state(syntax::ClearState::Pending);
-            }
-            if let Some(exchange) = entry.exchange {
-                let line_count = entry.line_count;
-                let spent = entry.spent.ok_or_else(|| {
-                    ImportError::Viseca(format!(
-                        "internal error: exchange should set aside with spent: {}",
-                        line_count
-                    ))
-                })?;
-                txn.add_rate(
-                    CommodityPair {
-                        source: exchange.equivalent.commodity,
-                        target: spent.commodity.clone(),
-                    },
-                    exchange.rate,
-                )?;
-                txn.transferred_amount(-spent);
-            } else if let Some(spent) = entry.spent {
-                txn.transferred_amount(-spent);
-            }
-            if let Some(fee) = entry.fee {
-                let payee = config.operator.as_ref().ok_or(ImportError::InvalidConfig(
-                    "config should have operator to have charge",
-                ))?;
-                txn.add_charge(payee, fee.amount);
-            }
-            result.push(txn);
         }
-        Ok(result)
+        let mut txn = single_entry::Txn::new(
+            entry.date,
+            payee,
+            OwnedAmount {
+                value: -entry.amount,
+                commodity: config.commodity.primary.clone(),
+            },
+        );
+        txn.effective_date(entry.effective_date)
+            .dest_account_option(fragment.account);
+        if !fragment.cleared {
+            txn.clear_state(syntax::ClearState::Pending);
+        }
+        if let Some(exchange) = entry.exchange {
+            let line_count = entry.line_count;
+            let spent = entry.spent.ok_or_else(|| {
+                ImportError::Viseca(format!(
+                    "internal error: exchange should set aside with spent: {}",
+                    line_count
+                ))
+            })?;
+            txn.add_rate(
+                CommodityPair {
+                    source: exchange.equivalent.commodity,
+                    target: spent.commodity.clone(),
+                },
+                exchange.rate,
+            )?;
+            txn.transferred_amount(-spent);
+        } else if let Some(spent) = entry.spent {
+            txn.transferred_amount(-spent);
+        }
+        if let Some(fee) = entry.fee {
+            let payee = config.operator.as_ref().ok_or(ImportError::InvalidConfig(
+                "config should have operator to have charge",
+            ))?;
+            txn.add_charge(payee, fee.amount);
+        }
+        result.push(txn);
     }
+    Ok(result)
 }
 
 #[derive(Debug, Clone, Copy)]
