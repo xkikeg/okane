c365025420ae1700b7800e8fa8c5e41d4755be80
kikeg <kikeg@kikeg.com>
2024-11-22 13:37:31 +0100

    Fix SingleEntry to emit charge commodity rate.
    Now it has unified function to convert
    OwnedAmount -> BorrowedAmount -> expr::Amount -> PostingAmount.

--- cli/src/import/amount.rs
+++ cli/src/import/amount.rs
@@ -1,3 +1,5 @@
+//! Defines extra amount types useful for import.
+
 use rust_decimal::Decimal;
 
 /// Represents simple owned Amount.
@@ -7,36 +9,46 @@ pub struct OwnedAmount {
     pub commodity: String,
 }
 
-impl OwnedAmount {
-    /// Returns `true` if the amount is zero.
-    pub fn is_zero(&self) -> bool {
-        self.value.is_zero()
-    }
-
-    /// Returns `true` if the amount is positive.
-    pub fn is_sign_positive(&self) -> bool {
-        self.value.is_sign_positive()
-    }
-
-    /// Returns `true` if the amount is negative.
-    pub fn is_sign_negative(&self) -> bool {
-        self.value.is_sign_negative()
+impl std::ops::Neg for OwnedAmount {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Self {
+            value: -self.value,
+            commodity: self.commodity,
+        }
     }
 }
 
-/// # Examples
-///
-/// ```
-/// # use rust_decimal_macros::dec;
-/// let x = okane::import::amount::OwnedAmount{
-///     value: dec!(-5),
-///     commodity: "JPY".to_string(),
-/// };
-/// let y = -x.clone();
-/// assert_eq!(y.value, dec!(5));
-/// assert_eq!(y.commodity, "JPY");
-/// ```
-impl std::ops::Neg for OwnedAmount {
+/// AmountRef unifies [OwnedAmount] reference and [BorrowedAmount].
+pub trait AmountRef<'a> {
+    fn into_borrowed(self) -> BorrowedAmount<'a>;
+}
+
+impl<'a> AmountRef<'a> for &'a OwnedAmount {
+    fn into_borrowed(self) -> BorrowedAmount<'a> {
+        BorrowedAmount {
+            value: self.value,
+            commodity: &self.commodity,
+        }
+    }
+}
+
+/// Represents a reference to [OwnedAmount].
+/// it's actually pretty close to [okane_core::syntax::expr::Amount],
+/// but without any formatting nor Cow.
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct BorrowedAmount<'a> {
+    pub value: Decimal,
+    pub commodity: &'a str,
+}
+
+impl<'a> AmountRef<'a> for BorrowedAmount<'a> {
+    fn into_borrowed(self) -> BorrowedAmount<'a> {
+        self
+    }
+}
+
+impl<'a> std::ops::Neg for BorrowedAmount<'a> {
     type Output = Self;
     fn neg(self) -> Self {
         Self {
