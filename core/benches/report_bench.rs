0baeb13ee9190fe4980c90fb1908f81c98f356bb
kikeg <kikeg@kikeg.com>
2025-01-26 14:07:05 +0100

    Added benchmark for report::query functions.

--- core/benches/report_bench.rs
+++ core/benches/report_bench.rs
@@ -26,7 +26,7 @@ fn load_benchmark(c: &mut Criterion) {
     });
 }
 
-fn report_benchmark(c: &mut Criterion) {
+fn report_process_benchmark(c: &mut Criterion) {
     let opts = report::ProcessOptions::default();
     let input = testing::ExampleInput::new(Path::new("report_bench")).unwrap();
     c.bench_function("process", |b| {
@@ -44,10 +44,56 @@ fn report_benchmark(c: &mut Criterion) {
     });
 }
 
+fn query_postings(c: &mut Criterion) {
+    let input = testing::ExampleInput::new(Path::new("report_bench")).unwrap();
+    let arena = Bump::new();
+    let mut ctx = report::ReportContext::new(&arena);
+    let opts = report::ProcessOptions::default();
+    let ledger = report::process(
+        &mut ctx,
+        load::new_loader(input.rootpath().to_owned()),
+        &opts,
+    )
+    .expect("report::process must succeed");
+
+    c.bench_function("query-posting-one-account", |b| {
+        b.iter(|| {
+            let query = report::query::PostingQuery {
+                account: Some("Assets:Account02".to_string()),
+            };
+            black_box(ledger.postings(&ctx, &query));
+        })
+    });
+}
+fn query_balance(c: &mut Criterion) {
+    let input = testing::ExampleInput::new(Path::new("report_bench")).unwrap();
+    let arena = Bump::new();
+    let mut ctx = report::ReportContext::new(&arena);
+    let opts = report::ProcessOptions::default();
+    let ledger = report::process(
+        &mut ctx,
+        load::new_loader(input.rootpath().to_owned()),
+        &opts,
+    )
+    .expect("report::process must succeed");
+
+    c.bench_function("query-balance-default", |b| {
+        b.iter(|| {
+            black_box(ledger.balance());
+        })
+    });
+}
+
 #[ctor::ctor]
 fn init() {
     let _ = env_logger::builder().is_test(true).try_init();
 }
 
-criterion_group!(benches, load_benchmark, report_benchmark);
+criterion_group!(
+    benches,
+    load_benchmark,
+    report_process_benchmark,
+    query_postings,
+    query_balance
+);
 criterion_main!(benches);
