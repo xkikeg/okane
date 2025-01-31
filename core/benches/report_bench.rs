98151b13b783080d379353ab26bfd7d3957914fd
kikeg <kikeg@kikeg.com>
2025-01-31 20:32:58 +0100

    Add repot_bench to test with on-memory files.
    This will enable running benches with different sized inputs.

--- core/benches/report_bench.rs
+++ core/benches/report_bench.rs
@@ -3,19 +3,46 @@ use std::path::Path;
 use bumpalo::Bump;
 use chrono::NaiveDate;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use log::LevelFilter;
 use okane_core::{
-    load::{self, LoadError},
-    report, syntax,
+    load::LoadError,
+    report::{self},
+    syntax,
 };
+use pretty_assertions::assert_eq;
+use testing::{ExampleInput, FakeFileSink, FileSink, RealFileSink};
 
 pub mod testing;
 
 fn load_benchmark(c: &mut Criterion) {
-    let input = testing::ExampleInput::new(Path::new("report_bench")).unwrap();
-    c.bench_function("load-with-counter", |b| {
+    let input = FakeFileSink::new_example(Path::new("report_bench")).unwrap();
+
+    basic_asserts(&input);
+
+    c.bench_function("load-on-memory", |b| {
         b.iter(|| {
             let mut count = 0;
-            load::new_loader(input.rootpath().to_owned())
+            input
+                .new_loader()
+                .load(|_, _, _: &syntax::tracked::LedgerEntry| {
+                    count += 1;
+                    Ok::<(), LoadError>(())
+                })
+                .unwrap();
+            black_box(());
+            black_box(count)
+        })
+    });
+
+    let input = RealFileSink::new_example(Path::new("report_bench")).unwrap();
+
+    basic_asserts(&input);
+
+    c.bench_function("load-on-file", |b| {
+        b.iter(|| {
+            let mut count = 0;
+            input
+                .new_loader()
                 .load(|_, _, _: &syntax::tracked::LedgerEntry| {
                     count += 1;
                     Ok::<(), LoadError>(())
@@ -28,34 +55,27 @@ fn load_benchmark(c: &mut Criterion) {
 }
 
 fn report_process_benchmark(c: &mut Criterion) {
+    let input = FakeFileSink::new_example(Path::new("report_bench")).unwrap();
     let opts = report::ProcessOptions::default();
-    let input = testing::ExampleInput::new(Path::new("report_bench")).unwrap();
+
     c.bench_function("process", |b| {
         b.iter(|| {
             let arena = Bump::new();
             let mut ctx = report::ReportContext::new(&arena);
-            let ret = report::process(
-                &mut ctx,
-                load::new_loader(input.rootpath().to_owned()),
-                &opts,
-            )
-            .expect("report::process must succeed");
+            let ret = report::process(&mut ctx, input.new_loader(), &opts)
+                .expect("report::process must succeed");
             black_box(ret);
         })
     });
 }
 
 fn query_postings(c: &mut Criterion) {
-    let input = testing::ExampleInput::new(Path::new("report_bench")).unwrap();
+    let input = FakeFileSink::new_example(Path::new("report_bench")).unwrap();
     let arena = Bump::new();
     let mut ctx = report::ReportContext::new(&arena);
     let opts = report::ProcessOptions::default();
-    let ledger = report::process(
-        &mut ctx,
-        load::new_loader(input.rootpath().to_owned()),
-        &opts,
-    )
-    .expect("report::process must succeed");
+    let ledger =
+        report::process(&mut ctx, input.new_loader(), &opts).expect("report::process must succeed");
 
     c.bench_function("query-posting-one-account", |b| {
         b.iter(|| {
@@ -67,16 +87,12 @@ fn query_postings(c: &mut Criterion) {
     });
 }
 fn query_balance(c: &mut Criterion) {
-    let input = testing::ExampleInput::new(Path::new("report_bench")).unwrap();
+    let input = FakeFileSink::new_example(Path::new("report_bench")).unwrap();
     let arena = Bump::new();
     let mut ctx = report::ReportContext::new(&arena);
     let opts = report::ProcessOptions::default();
-    let mut ledger = report::process(
-        &mut ctx,
-        load::new_loader(input.rootpath().to_owned()),
-        &opts,
-    )
-    .expect("report::process must succeed");
+    let mut ledger =
+        report::process(&mut ctx, input.new_loader(), &opts).expect("report::process must succeed");
 
     c.bench_function("query-balance-default", |b| {
         b.iter(|| {
@@ -134,9 +150,26 @@ fn query_balance(c: &mut Criterion) {
     });
 }
 
+fn basic_asserts<T: FileSink>(input: &ExampleInput<T>) {
+    let arena = Bump::new();
+    let mut ctx = report::ReportContext::new(&arena);
+    let opts = report::ProcessOptions::default();
+    let ledger =
+        report::process(&mut ctx, input.new_loader(), &opts).expect("report::process must succeed");
+    let num_txns = ledger.transactions().count();
+
+    assert_eq!(testing::num_transactions(), num_txns as u64);
+}
+
 #[ctor::ctor]
 fn init() {
-    let _ = env_logger::builder().is_test(true).try_init();
+    // print INFO level logs by default, unless overridden by env.
+    let mut builder = env_logger::builder();
+    builder
+        .is_test(true)
+        .filter_level(LevelFilter::Info)
+        .parse_default_env();
+    let _ = builder.try_init();
 }
 
 criterion_group!(
