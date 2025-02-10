8b1c90ef3955008d9d59d2ea424da7a8200db210
kikeg <kikeg@kikeg.com>
2025-02-10 14:13:19 +0100

    Add converted_amount to processed Posting.
    This will be useful to implement -X option for the balance later.

--- core/src/report/transaction.rs
+++ core/src/report/transaction.rs
@@ -1,10 +1,14 @@
 use chrono::NaiveDate;
 
-use super::{context::Account, eval::Amount};
+use super::{
+    context::Account,
+    eval::{Amount, SingleAmount},
+};
 
 /// Evaluated transaction, already processed to have right balance.
 // TODO: Rename it to EvaluatedTxn?
 #[derive(Debug, PartialEq, Eq)]
+#[non_exhaustive]
 pub struct Transaction<'ctx> {
     pub date: NaiveDate,
     // Posting in the transaction.
@@ -16,9 +20,18 @@ pub struct Transaction<'ctx> {
 
 /// Evaluated posting of the transaction.
 #[derive(Debug, Clone, PartialEq, Eq)]
+#[non_exhaustive]
 pub struct Posting<'ctx> {
+    /// Account of the posting.
     pub account: Account<'ctx>,
-    /// Note this Amount is not PostingAmount,
-    /// as deduced posting may have non-single commodity amount.
+
+    /// Amount of the posting described in the Ledger.
+    // Note this is not PostingAmount,
+    // as deduced posting may have non-single commodity amount.
     pub amount: Amount<'ctx>,
+
+    /// Amount of the posting in cost basis.
+    /// Some time this is useful for a few use cases:
+    /// - To balance within the transaction, we prefer this amount.
+    pub converted_amount: Option<SingleAmount<'ctx>>,
 }
