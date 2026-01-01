9c9c83b85c06376a56c5199f457dfdca0ca04a13
kikeg <kikeg@kikeg.com>
2026-01-01 23:21:45 +0100

    Implemented --current option to balance conversion.
    This allows easy query checking the current balance of assets /
    liabilities.
    Also renamed --now to --today given it's just 'day' precision.

--- cli/src/bin/okane.rs
+++ cli/src/bin/okane.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 use clap::Parser as _;
 
 use okane::cmd;
@@ -5,8 +7,11 @@ use okane::cmd;
 fn main() {
     env_logger::init();
     let cli = cmd::Cli::parse();
+    if let Err(err) = cli.validate() {
+        eprintln!("{}", err);
+        std::process::exit(2);
+    }
     if let Err(err) = cli.run(&mut std::io::stdout().lock()) {
-        use std::error::Error;
         eprintln!("{}", err);
         let mut cur: &dyn Error = &err;
         while let Some(src) = cur.source() {
